dc302dd0630f400b00862f263dc9ee7c5f665e30
Idan <Bahar.idan91@gmail.com>
2025-05-09 17:18:47 +0300

    adding some interactive with the ball, make him increase his size by 50px each tap

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ const BASE_SIZE = 100;
 const INCREMENT = 50;
 
 function onBallClick() {
-  console.log('hello');
+  let currentSize = parseInt(getComputedStyle(ball).width);
+
+  const newSize = currentSize + INCREMENT;
+  ball.style.width = newSize + 'px';
+  ball.style.height = newSize + 'px';
+  ball.textContent = newSize;
 }
+
+console.log(ball.style.width);
